889072b6048eff64c87cd4ae2372e29167a2d250
sizonzhao <sizonzhao@tencent.com>
2022-07-19 20:21:58 +0800

    最大无重复字符数目题目

--- src/string/LengthOfLongestSubstring.java
+++ src/string/LengthOfLongestSubstring.java
@@ -20,7 +20,7 @@ public class LengthOfLongestSubstring {
         }
         int num = 0;
         StringBuilder result = new StringBuilder();
-        for (int i=0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             String cStr = String.valueOf(c);
             if (result.toString().contains(cStr)) {
@@ -43,21 +43,21 @@ public class LengthOfLongestSubstring {
     public static int lengthOfLongestSubstring2(String s) {
         // 记录字符上一次出现的位置
         int[] last = new int[128];
-        for(int i = 0; i < 128; i++) {
+        for (int i = 0; i < 128; i++) {
             last[i] = -1;
         }
         int n = s.length();
 
         int res = 0;
         int start = 0; // 窗口开始位置
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             int index = s.charAt(i);
             System.out.print("index: " + index);
             // 计算起始位置，比如当前字符没出现过，那么last[index] + 1肯定为0，如果出现过，但是比start的位置靠前，那么起始位置还是start
             // 如果出现过，而且比start靠后，那么起始位置就变成了last[index] + 1，加1是因为不能重复，把前面出现过的那个字符去掉
             start = Math.max(start, last[index] + 1);
             // 这里就是算当前最大长度和新的最大长度哪个更大一点，留更大的
-            res   = Math.max(res, i - start + 1);
+            res = Math.max(res, i - start + 1);
             // 更新一下该字符出现的位置，下次计算以新位置为基准
             last[index] = i;
         }
@@ -74,11 +74,11 @@ public class LengthOfLongestSubstring {
 
         int res = 0;
         int start = 0; // 窗口开始位置
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             int index = s.charAt(i);
             start = Math.max(start, last[index]);
-            res   = Math.max(res, i - start + 1);
-            last[index] = i+1;
+            res = Math.max(res, i - start + 1);
+            last[index] = i + 1;
         }
 
         return res;
